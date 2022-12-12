5c791f5fde9c348560191abe609460876f451773
Joshuah Ciafardone <joshuah.ciafardone@gmail.com>
2022-12-12 15:15:30 -0800

    Generate secret number

--- game.py
+++ game.py
@@ -1,6 +1,11 @@
 """A number-guessing game."""
 
 # Put your code here
+import random
+
 print("Welcome to the Guessing Game!")
 
 player_name = input("What is your name? ")
+ 
+number = random.randint(1, 101)
+print(number)
